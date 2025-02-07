fee4b492aa4793c1849a52ff324f2d27ae0a2bf9
Fauzan <fauzandwimulyono@gmail.com>
2025-02-07 12:36:03 +0700

    Change split to chars

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -38,7 +38,7 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split('')
+  string_array = string.downcase.chars
   unique_length = string_array.uniq.length
   original_length == unique_length
 end
